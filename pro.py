7d88c7065a69eb1ab0213e67e1e828d6d2eff036
LakatosGizi <alexszombat44@gmail.com>
2025-09-05 12:16:10 +0200

    done

--- pro.py
+++ pro.py
@@ -1,1 +1,27 @@
-print("Szia")
+print("Szia" , end=" ")
+print("Tibi")
+print("Szia", "Tóth" , "Karcsi" , "!" , sep="")
+print("Szia tóth karcsi!")
+nev="tóth karcsi"
+print(f"szia {nev}!")
+print("szia", "peti" , sep="\n", end="!") #\t
+#adattipusok
+# int()
+# float()
+# str()
+# bool()
+# list()
+# set()
+print()
+print(int("5"))
+print(float("5"))
+print(type(str("5.0")))
+print(bool(1))
+print(["hétfő", "kedd", "szerda"])
+nevek=["tibi","sanyi","tibi"]
+print(f"nevek: {nevek}")
+print(tuple([1,2,3]))
+#hf:git parancsok:
+#git init:adott mappa verziókövetésének inditása
+#git add. :
+
