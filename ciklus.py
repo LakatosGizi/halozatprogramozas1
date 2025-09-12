c00f1b5eea05303859a6f88226fe202cd994dd3a
LakatosGizi <alexszombat44@gmail.com>
2025-09-12 12:33:50 +0200

    szorzótábla

--- ciklus.py
+++ ciklus.py
@@ -9,10 +9,35 @@ def osszegez(lista):
     return osszeg
 print(f"a lista elemeinek összege:{osszegez(lista)}")
 
-def paros_szam(lista):
-    osszeg=0
+def paros_db(lista):
+    darab=0
     for elem in lista:
-        if elem%2==0:
-            darab+=1
+        if elem % 2 == 0:
+            darab += 1
     return darab
-print(f"A lista páros elemeinek darabszáma:{paros_szam(lista)}")
+print(f"A lista páros elemeinek darabszáma:{paros_db(lista)}")
+
+print("*"*5)
+for i in range(5):
+    print("*", end="")
+print()
+
+magassag = 5
+
+for i in range(1, magassag + 1):
+    sor = ' ' * (magassag - i) + '*' * (2 * i - 1)
+    print(sor)
+
+n=9
+space_db=n-1
+csillag_db=1
+for i in range(n):
+    print(" "* space_db,end="")
+    print("*"* csillag_db)
+    space_db-=1
+    csillag_db+=2
+
+for i in range(1,11):
+    for j in range(1,11):
+        print(f"{i*j:>4}",end="")
+    print()
