391ccd950bf03104288dc47edc4bfc7b67d31825
LakatosGizi <alexszombat44@gmail.com>
2025-09-12 10:32:40 +0200

    tesztelés

--- elagazas.py
+++ elagazas.py
@@ -12,3 +12,25 @@ elif gondolt_szam > bekert_szam:
 
 else:
     print("kisebbre gondoltam")
+
+#készits fűggvényt elöjel néven mely átvesz egy egész számot és az elöjelét adja vissza
+def elojel(szam):
+    if szam>0:
+        return"+"
+    else:
+        return"-"
+szam=int(input("kérek egy számot elöjellel: "))
+print(f"A{szam}elojele: {elojel(szam)}.")
+print(f"a {szam}elojele: {'+' if szam>0 else '-'}")
+def tesztesetek():
+    teszt(5,"+")
+    teszt(-3,"-")
+    teszt(-1,"-")
+    teszt(0,"+")
+    teszt(1,"+")
+def teszt(szam,elvart_elojel):
+    if elojel(szam)==elvart_elojel:
+        print(f"elojel({szam})=={elvart_elojel}ture")
+    else:
+        print(f"elojel({szam})=={elvart_elojel}false")
+tesztesetek()
