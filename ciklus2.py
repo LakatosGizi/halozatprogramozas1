f0ac219177b9f694ccbaec4110bb60aa3177b690
LakatosGizi <alexszombat44@gmail.com>
2025-09-12 12:59:08 +0200

    számbekérés

---
+++ ciklus2.py
@@ -0,0 +1,9 @@
+#kérjűnk be számokat és adjuk meg az összegűket
+# 1 0 végjelig
+#2 enter végjelig
+osszeg=0
+be_n=1
+while be_n !=0:
+    be_n=int(input("kérek egy számot: "))
+    osszeg=osszeg+be_n
+print=(osszeg)
